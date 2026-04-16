0d7c76a1dcc480b0755cc10d59390b02a72b5789
Shadipto Pranto <shadipto.pranto@gmail.com>
2026-04-16 11:53:54 +0600

    php

---
+++ php_class/practice.php
@@ -0,0 +1,108 @@
+<?php
+// 1. Indexed array of student marks
+$marks = array(65, 42, 78, 90, 55);
+
+// foreach
+echo "<h3>Student Marks:</h3>";
+foreach ($marks as $m) {
+    echo $m . "<br>";
+}
+
+// 2. Calculate total, average, max, min
+$total = 0;
+$max = $marks[0];
+$min = $marks[0];
+
+foreach ($marks as $m) {
+    $total += $m;
+
+    if ($m > $max) {
+        $max = $m;
+    }
+
+    if ($m < $min) {
+        $min = $m;
+    }
+}
+
+// Type casting (explicit)
+$count = (int) count($marks);
+
+// functions
+function calculateAverage($total, $count) {
+    return $total / $count;
+}
+
+$average = calculateAverage($total, $count);
+
+echo "<h3>Statistics:</h3>";
+echo "Total: $total <br>";
+echo "Average: $average <br>";
+echo "Maximum: $max <br>";
+echo "Minimum: $min <br>";
+
+
+// pass and fail
+$pass = 0;
+$fail = 0;
+
+// check pass/fail
+function isPass($mark) {
+    if ($mark >= 50) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+foreach ($marks as $m) {
+    if (isPass($m)) {
+        $pass++;
+    } else {
+        $fail++;
+    }
+}
+
+echo "<h3>Result:</h3>";
+echo "Passed Students: $pass <br>";
+echo "Failed Students: $fail <br>";
+
+// Associative array
+$student = array(
+    "name" => "Dipto",
+    "id" => "12345",
+    "cgpa" => 3.94
+);
+
+echo "<h3>Student Details:</h3>";
+foreach ($student as $key => $value) {
+    echo "$key : $value <br>";
+}
+
+// String operations
+$name = $student["name"];
+$upperName = strtoupper($name); // convert to uppercase
+$nameLength = strlen($name);    // string length
+
+echo "<h3>String Operations:</h3>";
+echo "Original Name: $name <br>";
+echo "Uppercase Name: $upperName <br>";
+echo "Name Length: $nameLength <br>";
+
+
+// array function (sort)
+sort($marks);
+
+echo "<h3>Sorted Marks:</h3>";
+foreach ($marks as $m) {
+    echo $m . "<br>";
+}
+
+
+// Superglobal ($_GET)
+$userInput = isset($_GET['username']) ? $_GET['username'] : "Guest";
+
+echo "<h3>User Input:</h3>";
+echo "Hello, " . htmlspecialchars($userInput) . "!";
+
+?>
